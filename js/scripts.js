66599e686fc6242a1281b059e2f99f640bb42d3e
Luke Chinworth <lukechinworth@gmail.com>
2015-02-10 10:32:14 -0800

    add capitalization to first word of title

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var titleCase = function(sentence) {
   var answer = [];
   var noCaps = ["the", "and", "a", "an", "in", "but", "or", "with", "upon"];
   var lowerCaseSentence = sentence.toLowerCase();
-  lowerCaseSentence.split(" ").forEach(function(word) {
+  var wordArray = lowerCaseSentence.split(" ");
+  var firstWord = wordArray.shift();
+  answer.push(firstWord[0].toUpperCase() + firstWord.slice(1));
+  wordArray.forEach(function(word) {
     if (noCaps.indexOf(word) === -1) {
       answer.push(word[0].toUpperCase() + word.slice(1));
     } else {
